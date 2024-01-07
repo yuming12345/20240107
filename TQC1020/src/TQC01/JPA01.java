6b99f4ef2c65c93f1073ce84c8e001d62ccde8fb
yuming12345 <95611008+yuming12345@users.noreply.github.com>
2024-01-07 14:34:02 +0800

    Add files via upload

---
+++ TQC1020/src/TQC01/JPA01.java
@@ -0,0 +1,11 @@
+package TQC01;
+
+public class JPA01 {
+
+	public static void main(String[] args) {
+		System.out.println("I love Java!");
+        System.out.println("Java is so good!");
+
+	}
+
+}
---
+++ TQC1020/src/TQC01/JPA02.java
@@ -0,0 +1,12 @@
+package TQC01;
+import java.util.Scanner;
+public class JPA02 {
+
+	public static void main(String[] args) {
+		System.out.printf("[Please input:");
+		double p = new Scanner(System.in).nextDouble();
+		System.out.printf("%f kg=%f ponds",p, p*2.20462);
+
+	}
+
+}
